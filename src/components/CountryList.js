d624c91d91077278166a095e7705b1caf7df2e9f
Tom Dolton <tom@Toms-MBP.lan>
2020-06-22 17:17:58 +0100

    Replace icons with SVGs

--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { ThemeContext } from "../contexts/ThemeContext";
 import { CountriesContext } from "../contexts/CountriesContext";
